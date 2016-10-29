91d54a24b216b3c10594abf055e3f9819e3a753e
Brian Blosser <brian@slamagency.com>
2016-10-29 16:57:04 -0500

    Got rid of some Foundation wrapper stuff that wasn't needed.

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -19,8 +19,6 @@
 						</nav>
 					</footer> <!-- end .footer -->
 				</div>  <!-- end .main-content -->
-			</div> <!-- end .off-canvas-wrapper-inner -->
-		</div> <!-- end .off-canvas-wrapper -->
 		<?php wp_footer(); ?>
 	</body>
 </html> <!-- end page -->
--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -4,15 +4,15 @@
 
 	<head>
 		<meta charset="utf-8">
-		
+
 		<!-- Force IE to use the latest rendering engine available -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 		<!-- Mobile Meta -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta class="foundation-mq">
-		
-		
+
+
 		<!-- Icons & Favicons -->
 		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 		<!--<link href="<?php //echo get_template_directory_uri(); ?>/assets/images/apple-icon-touch.png" rel="apple-touch-icon" /> -->
@@ -22,7 +22,7 @@
 		<!-- <meta name="msapplication-TileColor" content="#f01d4f"> -->
 		<!--<meta name="msapplication-TileImage" content="<?php //echo get_template_directory_uri(); ?>/assets/images/win8-tile-icon.png">-->
     	<!-- <meta name="theme-color" content="#121212"> -->
-	    
+
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
@@ -42,36 +42,30 @@
 		<?php wp_head(); ?>
 
 		<!-- Drop Google Analytics here -->
-		<?php 
-			$analytics_script = get_theme_mod('google_analytics_code_snippet'); 
-			//error_log('--------------------$analytics_script------------------------'); 
+		<?php
+			$analytics_script = get_theme_mod('google_analytics_code_snippet');
+			//error_log('--------------------$analytics_script------------------------');
 			//error_log( var_export($analytics_script, true) );
 			echo $analytics_script;
 		?>
 		<!-- end analytics -->
-		
+
 		<!--Font Awesome CDN -->
 		<script src="https://use.fontawesome.com/645de6072f.js"></script>
 		<!--End Font Awesome CDN-->
 
 	</head>
-	
-	<!-- Uncomment this line if using the Off-Canvas Menu --> 
-		
+
+	<!-- Uncomment this line if using the Off-Canvas Menu -->
+
 	<body <?php body_class(); ?>>
 
-		<div class="off-canvas-wrapper">
-			
-			<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-				
-				<?php get_template_part( 'parts/content', 'offcanvas' ); ?>
-				
 				<div class="off-canvas-content" data-off-canvas-content>
-					
+
 					<header class="header" role="banner">
-							
-						 <!-- This navs will be applied to the topbar, above all content 
+
+						 <!-- This navs will be applied to the topbar, above all content
 							  To see additional nav styles, visit the /parts directory -->
 						 <?php get_template_part( 'parts/nav', 'vertical-overlay' ); ?>
-		 	
+
 					</header> <!-- end .header -->
