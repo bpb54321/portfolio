bbb343cd1855f7dc7991a751ab876f52bc49f947
Brian Blosser <brian@slamagency.com>
2016-11-05 17:32:49 +0800

    Added the title below the image link.

--- wp-content/themes/portfolio/front-page.php
+++ wp-content/themes/portfolio/front-page.php
@@ -30,38 +30,30 @@
 		$num_project_rows = ceil( $total_available_projects/$num_projects_row_medium ) ;
 
 		$num_columns_project = 12 / $num_projects_row_medium;
+		?>
 
-		echo "<section class='project-gallery' data-project-count='{$total_available_projects}'>";
-			for ( $project_count = 0, $row_count = 0; $project_count < $total_available_projects; $project_count++, $row_count = floor($project_count/$num_projects_row_medium) ) {
-				if ( $project_count % $num_projects_row_medium == 0 ) {
-					echo "<div class='row'>";
-				}
-						echo "<div class='small-12 medium-{$num_columns_project} columns project-thumbnail-container'>";
-							$project = $projects_query->posts[$project_count];
-							$image_src_string = get_featured_image_src( $project, 'large' );
-							$project_permalink = get_permalink( $project );
-
-								$project_post_category_string = get_post_category_string($project);
-								$project_grid_tile_html =
-
-								"<div class='project-thumbnail project-thumbnail-{$project_count}' style='background-image: url( {$image_src_string} )'>
-									<a href='{$project_permalink}'>
-										<div class='project-thumbnail-overlay'>
-											<h5 class='project-title'>{$project->post_title}</h5>
-											<hr>
-											<h5 class='project-category'>{$project_post_category_string}</h5>
-										</div>
-									</a>
-								</div>";
-
-							echo $project_grid_tile_html;
-						echo "</div>";
-				if ( $project_count%$num_projects_row_medium==($num_projects_row_medium-1) ) {
-					echo "</div><!--End .row-->";
-				}
-			}
-		echo "</section>";
-	?>
+		<section class='project-gallery' data-project-count='<?=$total_available_projects?>'>
+			<?php for ( $project_count = 0, $row_count = 0; $project_count < $total_available_projects; $project_count++, $row_count = floor($project_count/$num_projects_row_medium) ) : ?>
+				<?php
+					$project = $projects_query->posts[$project_count];
+					$image_src_string = get_featured_image_src( $project, 'large' );
+					$project_permalink = get_permalink( $project );
+				?>
+				<?php if ( $project_count % $num_projects_row_medium == 0 ) : ?>
+					<div class='row'>
+				<?php endif; ?>
+						<div class='small-12 medium-<?=$num_columns_project?> columns project-thumbnail-container'>
+							<a href='<?=$project_permalink?>'>
+								<div class='project-thumbnail project-thumbnail-<?=$project_count?>' style='background-image: url( <?=$image_src_string?> )'>
+							  </div>
+						  </a>
+							<h5 class='project-title'><?=$project->post_title?></h5>
+						</div>
+				<?php if ( $project_count%$num_projects_row_medium == ($num_projects_row_medium-1) ) : ?>
+					</div><!--End .row-->;
+				<?php endif; ?>
+			<?php endfor; ?>
+		</section>
 
 	<section class="summary-text">
 		<?php the_content(); ?>
