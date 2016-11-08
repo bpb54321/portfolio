ad55d811d8a0c90f409c42263441e79d46f7737b
Brian Blosser <brian@slamagency.com>
2016-11-08 21:39:11 +0800

    Removed the footer.

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,22 +1,5 @@
 					<footer class="footer" role="contentinfo">
-						<nav role="navigation">
-							<div id="footer-precursor" class="row">
-								<hr>
-								<p>Visit the <strong>UIC</strong> Family of Companies</p>
-								<hr>
-							</div>
-							<div id="footer-top-row" class="row">
-			    				<?php refine_top_footer_menu(); ?>
-		    				</div>
-		    				<div id="footer-middle-row" class="row">
-			    				<?php refine_middle_footer_menu(); ?>
-		    				</div>
-			    			<div id="footer-bottom-row" class="row">
-								<hr>
-								<?php refine_bottom_footer_menu(); ?>
-								<hr>
-							</div>
-						</nav>
+						
 					</footer> <!-- end .footer -->
 				</div>  <!-- end .main-content -->
 		<?php wp_footer(); ?>
