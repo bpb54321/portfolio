ceef67a46cefdf4a6036e9d3e74f9595f353a78a
Brian Blosser <brian@slamagency.com>
2016-11-08 18:19:15 +0800

    Removed the existing javascript.

--- wp-content/themes/portfolio/assets/js/scripts/project-overlay.js
+++ wp-content/themes/portfolio/assets/js/scripts/project-overlay.js
@@ -1,13 +1,3 @@
 jQuery(document).ready(function() {
 
-	var $projectThumbnails = jQuery(".project-thumbnail");
-  $projectThumbnails.on("mouseover", function() {
-    jQuery(this).addClass("lightened-image");
-    jQuery(this).removeClass("normal-image");
-  });
-  $projectThumbnails.on("mouseleave", function() {
-    jQuery(this).addClass("normal-image");
-    jQuery(this).removeClass("lightened-image");
-  });
-
 });
