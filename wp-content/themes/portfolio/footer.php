3efd1baf17ff1cc7f9da13c7cbdd1b23e08b6c98
Brian Blosser <brian@slamagency.com>
2016-11-11 10:31:17 +0800

    Added new footer.

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,5 +1,11 @@
 					<footer class="footer" role="contentinfo">
-						
+						<div class="row">
+							<div class="small-12 columns">
+								<p>
+									Developed by <span class="bold">Brian Blosser</span>
+								</p>
+							</div>
+						</div>
 					</footer> <!-- end .footer -->
 				</div>  <!-- end .main-content -->
 		<?php wp_footer(); ?>
