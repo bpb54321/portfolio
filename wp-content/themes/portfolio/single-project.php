a639e874e7214ea666f1f49fbee2e9c07275244b
Brian Blosser <brian@slamagency.com>
2016-11-11 14:13:24 +0800

    Beginning of redoing the single-project template.

--- wp-content/themes/portfolio/single-project.php
+++ wp-content/themes/portfolio/single-project.php
@@ -1,76 +1,30 @@
 <?php get_header(); ?>
+<?php
+	$ftd_img_id = get_post_thumbnail_id ( $post );
+	$ftd_img_default_src = wp_get_attachment_image_src( $ftd_img_id, 'large');
+	$ftd_img_srcset = wp_get_attachment_image_srcset( $ftd_img_id, $size = 'medium', $image_meta = null );
+	// error_log('-----------------------------------$ftd_img_srcset----------------------------------------');
+	// error_log( print_r($ftd_img_srcset, true) );
+	// error_log('-----------------------------------$ftd_img_srcset----------------------------------------');
+	//
+	// error_log('-----------------------------------$ftd_img_metadata----------------------------------------');
+	// error_log( print_r($ftd_img_metadata, true) );
+	// error_log('-----------------------------------$ftd_img_metadata----------------------------------------');
+ ?>
 
-<?php get_template_part( 'parts/header', 'hero-image' ); ?>
-
-<?php 
-	$slam_project_year = get_post_meta($post->ID, 'slam_project_year', true);
-	$project_categories = get_post_category_string($post);
-	//$link_file_url = get_post_meta($post->ID, 'link_to_file_media_file', true);
-	//$link_file_text = get_post_meta($post->ID, 'link_to_file_text', true);
-	$link_to_file_link_group = get_post_meta($post->ID, 'link_to_file_link_group', false);
-
-
-	//error_log('------------------$link_file_url----------------');
-	//error_log( print_r($link_file_url, true) );
-
-	//error_log('------------------$link_file_text----------------');
-	//error_log( print_r($link_file_text, true) );
-
-	$link_to_file_link_group = $link_to_file_link_group[0];
-
-	//error_log('------------------$link_to_file_link_group----------------');
-	//error_log( print_r($link_to_file_link_group, true) );
-
-
-
-?>
-
-<section class="content">
-
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-		<article class="main-article">
-			<h3 class="title"><?php the_title(); ?></h3>
-			<?php if( $project_categories !== false || $slam_project_year !== '' ) : ?>
-				<div class="categories">
-					<?php if( $project_categories !== false ) {
-						echo "<h3 class='single-project-category-string'>";
-						echo get_post_category_string($post); //Defined in functions.php
-						echo "</h3>";
-					}
-					?>
-					<?php if( $slam_project_year !== '' ) {
-						echo "<h3 class='single-project-year-string'>";
-						echo $slam_project_year;
-						echo "</h3>";
-					}
-					?>
-				</div>
-			<?php endif; ?>
-			<?php 
-				//if( $link_file_url !== '' ) {
-				if ( sizeof( $link_to_file_link_group ) > 0 ) : ?>
-					<div class='link-to-file'>
-					<?php foreach ( $link_to_file_link_group as $link_array ) : ?>
-						<a target='_blank' href='<?php echo $link_array["link_to_file_media_file"]; ?>'><?php echo $link_array["link_to_file_text"]; ?></a>
-					<?php endforeach; ?>
-					</div>
-				<?php endif; ?>
-	    	<?php the_content(); ?>
-		</article>
-		
-		<?php get_template_part('parts/components', 'image-slider'); ?>
-    	
-    <?php endwhile; else : ?>
-
-   		<?php get_template_part( 'parts/content', 'missing' ); ?>
-
-    <?php endif; ?>
-
-</section>
+<div class="row">
+	<div class="large-8 columns">
+		<img src="<?=$ftd_img_default_src?>" srcset="<?=$ftd_img_srcset?>" alt="Project Featured Image">
+	</div>
+	<div class="large-4 columns">
+		<div class="single-project-copy">
+			<h1 class="project-title">Project title</h1>
+			<p class="project-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<a>View Site</a>
+		</div>
+	</div>
+</div>
 
 <?php get_template_part( 'parts/footer', 'cta' ); ?>
 
 <?php get_footer(); ?>
-
-
