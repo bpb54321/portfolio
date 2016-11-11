3c39f3a5220e697d36807aafb6ea578e5e980b20
Brian Blosser <brian@slamagency.com>
2016-11-11 14:38:17 +0800

    Now loading the image, title, and content in the single-project.php template.

--- wp-content/themes/portfolio/single-project.php
+++ wp-content/themes/portfolio/single-project.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <?php
 	$ftd_img_id = get_post_thumbnail_id ( $post );
-	$ftd_img_default_src = wp_get_attachment_image_src( $ftd_img_id, 'large');
+	//$ftd_img_default_src = wp_get_attachment_image_src( $ftd_img_id, 'large');
 	$ftd_img_srcset = wp_get_attachment_image_srcset( $ftd_img_id, $size = 'medium', $image_meta = null );
 	// error_log('-----------------------------------$ftd_img_srcset----------------------------------------');
 	// error_log( print_r($ftd_img_srcset, true) );
@@ -14,12 +14,13 @@
 
 <div class="row">
 	<div class="large-8 columns">
-		<img src="<?=$ftd_img_default_src?>" srcset="<?=$ftd_img_srcset?>" alt="Project Featured Image">
+		<img srcset="<?=$ftd_img_srcset?>" sizes="(max-width: 1024px) 100vw, 75vw" alt="Project Featured Image">
 	</div>
 	<div class="large-4 columns">
 		<div class="single-project-copy">
-			<h1 class="project-title">Project title</h1>
-			<p class="project-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<h1 class="project-title"><?php echo $post->post_title; ?></h1>
+			<p class="project-description"><?php echo $post->post_content; ?></p>
+
 			<a>View Site</a>
 		</div>
 	</div>
