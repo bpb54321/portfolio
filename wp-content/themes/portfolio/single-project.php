ce90e55932452216a733bf43864358c446f43ef9
Brian Blosser <brian@slamagency.com>
2016-11-11 09:27:55 -0800

    Added target=_blank to the “Visit Site” links.

--- wp-content/themes/portfolio/single-project.php
+++ wp-content/themes/portfolio/single-project.php
@@ -5,25 +5,19 @@
 	$project_url = get_post_meta( $post->ID, 'project_details_url', true );
 
 	$prev_project_permalink = get_adjacent_custom_post_type( $post, 'project', 'before' );
-	error_log('-----------------------------------$prev_project_permalink----------------------------------------');
-	error_log( print_r($prev_project_permalink, true) );
-	error_log('-----------------------------------$prev_project_permalink----------------------------------------');
-
 	$next_project_permalink = get_adjacent_custom_post_type( $post, 'project', 'after' );
-	error_log('-----------------------------------$next_project_permalink----------------------------------------'); 
-	error_log( print_r($next_project_permalink, true) ); 
-	error_log('-----------------------------------$next_project_permalink----------------------------------------');
-?>
 
+?>
 <div class="row">
 	<div class="large-8 columns">
 		<img srcset="<?=$ftd_img_srcset?>" sizes="(max-width: 1024px) 100vw, 75vw" alt="Project Featured Image">
 	</div>
+
 	<div class="large-4 columns">
 		<div class="single-project-copy">
 			<h1 class="project-title"><?php echo $post->post_title; ?></h1>
 			<p class="project-details"><?php echo $post->post_content; ?></p>
-			<a href="<?=$project_url?>">View Site</a>
+			<a target="_blank" href="<?=$project_url?>">View Site</a>
 		</div>
 	</div>
 </div>
