866de3b048a107790f7fd18a48d19fbe51038d53
Brian Blosser <brian@slamagency.com>
2016-11-14 10:46:18 -0700

    Finished styling of the single-project template.

--- wp-content/themes/portfolio/parts/content-single.php
+++ wp-content/themes/portfolio/parts/content-single.php
@@ -4,7 +4,7 @@
 	$project_url = get_post_meta( $post->ID, 'project_details_url', true );
 ?>
 <div class="row main-content">
-  <div class="large-4 columns">
+  <div class="large-4 large-push-8 columns">
 		<div class="single-project-copy">
 			<h1 class="project-title"><?php echo $post->post_title; ?></h1>
 			<p class="project-details"><?php echo $post->post_content; ?></p>
@@ -13,7 +13,9 @@
       <?php endif; ?>
 		</div>
 	</div>
-  <div class="large-8 columns">
-		<img srcset="<?=$ftd_img_srcset?>" sizes="(max-width: 1024px) 100vw, 75vw" alt="Project Featured Image">
-	</div>
+  <div class="large-8 large-pull-4 columns">
+    <a href="<?=$project_url?>" class="image-link" style="display: block;">
+      <img srcset="<?=$ftd_img_srcset?>" sizes="(max-width: 1024px) 100vw, 75vw" alt="Project Featured Image">
+    </a>
+  </div>
 </div>
