a412ee80f0ca2af579079bd10ede9aec205d5aeb
Brian Blosser <brian@slamagency.com>
2016-11-18 14:51:32 -0600

    The javascript now hides the spinning wheel once the image of the background div is loaded.

--- wp-content/themes/portfolio/assets/js/scripts.js
+++ wp-content/themes/portfolio/assets/js/scripts.js
@@ -14,7 +14,8 @@ jQuery(document).ready(function() {
 (function( $ ) {
 
   $('.project-thumbnail').imagesLoaded( { background: true } ).progress( function( instance, image ) {
-    console.log('#container background images have loaded');
+    //After each background image is loaded, hide the overlay with the spinning progress wheel
+    $(image.element).children(".project-thumbnail-overlay").css("display", "none");
   });
 
 })( jQuery );
--- wp-content/themes/portfolio/assets/js/scripts/image-loading-animation-control.js
+++ wp-content/themes/portfolio/assets/js/scripts/image-loading-animation-control.js
@@ -1,7 +1,8 @@
 (function( $ ) {
 
   $('.project-thumbnail').imagesLoaded( { background: true } ).progress( function( instance, image ) {
-    console.log('#container background images have loaded');
+    //After each background image is loaded, hide the overlay with the spinning progress wheel
+    $(image.element).children(".project-thumbnail-overlay").css("display", "none");
   });
 
 })( jQuery );
